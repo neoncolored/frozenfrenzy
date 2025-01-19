7fab4566899528294f33741be053a20d39192cdd
Ar-da <arda.stigler@gmail.com>
2025-01-19 14:36:41 +0100

    Penguin Changes: - New walking anim - changed penguin size - normal speed diagonally
    Enemy Changes
    - speed auf 0 vorrübergehend

--- MyFancyUnityProject/Assets/Scripts/Player.cs
+++ MyFancyUnityProject/Assets/Scripts/Player.cs
@@ -6,15 +6,19 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator playerAnimator;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     
-    private Vector3 m_velocity = Vector3.zero;
+    
+    private Vector3 _velocity = Vector3.zero;
     public float speed = 10.0f;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -36,12 +40,16 @@ public class Player : MonoBehaviour
 
     private void PlayerInput()
     {
-        m_velocity.y = Input.GetAxis("Vertical") * speed;
-        m_velocity.x = Input.GetAxis("Horizontal") * speed;
+        _velocity.y = Input.GetAxis("Vertical") * speed;
+        _velocity.x = Input.GetAxis("Horizontal") * speed;
+        _velocity = Vector2.ClampMagnitude(_velocity, 50.0f); 
+        // maxLength muss immer = speed sein, damit man diagonal nicht schenller ist
     }
 
     private void PlayerMove()
     {
-        rb.velocity = m_velocity * Time.fixedDeltaTime;
+        _animator.SetFloat("speed", Math.Abs(_velocity.x));
+        if (_velocity.x != 0) _spriteRenderer.flipX = _velocity.x < 0;
+        _rigidbody2D.velocity = _velocity * Time.fixedDeltaTime;
     }
 }
