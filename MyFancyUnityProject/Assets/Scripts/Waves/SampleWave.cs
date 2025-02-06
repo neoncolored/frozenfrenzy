0d72d3c682cdf765cc14524f61c04c9ca433425a
pasca <go76sob@mytum.de>
2025-02-06 23:08:13 +0100

    Wave update

--- MyFancyUnityProject/Assets/Scripts/Waves/SampleWave.cs
+++ MyFancyUnityProject/Assets/Scripts/Waves/SampleWave.cs
@@ -35,55 +35,55 @@ namespace Waves
             {
                 case 1: //wie viele gegner in wave 1 spawnen
                 {
-                    numBat = 5;
-                    numGrinch = 0;
-                    numKrampus = 0;
-                    numSnowman = 5;
-                    numBoss = 0;
-                    break;
-                }
-                case 2: //in wave 2
-                {
                     numBat = 0;
-                    numGrinch = 5;
-                    numKrampus = 0;
-                    numSnowman = 0;
-                    numBoss = 0;
-                    break;
-                }
-                case 3: //usw.
-                {
-                    numBat = 10;
                     numGrinch = 0;
                     numKrampus = 10;
                     numSnowman = 0;
                     numBoss = 0;
                     break;
                 }
-                case 4:
+                case 2: //in wave 2
                 {
                     numBat = 5;
-                    numGrinch = 5;
-                    numKrampus = 5;
+                    numGrinch = 0;
+                    numKrampus = 10;
+                    numSnowman = 0;
+                    numBoss = 0;
+                    break;
+                }
+                case 3: //usw.
+                {
+                    numBat = 5;
+                    numGrinch = 0;
+                    numKrampus = 10;
                     numSnowman = 5;
+                    numBoss = 0;
+                    break;
+                }
+                case 4:
+                {
+                    numBat = 10;
+                    numGrinch = 0;
+                    numKrampus = 0;
+                    numSnowman = 10;
                     numBoss = 0;
                     break;
                 }
                 case 5:
                 {
-                    numBat = 5;
-                    numGrinch = 5;
-                    numKrampus = 15;
-                    numSnowman = 5;
+                    numBat = 0;
+                    numGrinch = 15;
+                    numKrampus = 0;
+                    numSnowman = 0;
                     numBoss = 0;
                     break;
                 }
                 case 6:
                 {
-                    numBat = 0;
-                    numGrinch = 0;
-                    numKrampus = 0;
-                    numSnowman = 25;
+                    numBat = 5;
+                    numGrinch = 5;
+                    numKrampus = 5;
+                    numSnowman = 5;
                     numBoss = 0;
                     break;
                 }
@@ -98,21 +98,22 @@ namespace Waves
                 }
                 case 8:
                 {
-                    numBat = 10;
-                    numGrinch = 10;
-                    numKrampus = 10;
-                    numSnowman = 10;
+                    numBat = 15;
+                    numGrinch = 15;
+                    numKrampus = 15;
+                    numSnowman = 15;
                     numBoss = 0;
                     break;
                 }
                 case 9:
                 {
-                    numBat = 10;
-                    numGrinch = 10;
-                    numKrampus = 10;
-                    numSnowman = 10;
+                    numBat = 20;
+                    numGrinch = 20;
+                    numKrampus = 20;
+                    numSnowman = 20;
                     numBoss = 0;
                     break;
+                    
                 }
                 case 10: //Boss
                 {
@@ -187,6 +188,10 @@ namespace Waves
             wave += 1;
             enemyCap += 2; //maybe something like numGrinch+=2
             isSet = false;
+            yield return new WaitForSeconds(60);
+            isSet = true;
+            SetWave();
+            StartCoroutine(StartWave());
         }
     
     
